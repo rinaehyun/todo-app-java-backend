13cab8933323c56bc507342b099a02f1284cba9b
Rinae Hyun <rnhyun.dream@gmail.com>
2024-07-26 10:07:44 +0200

    Initial commit

---
+++ src/main/java/org/example/todoappjavabackend/TodoAppJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.todoappjavabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppJavaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoAppJavaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/todoappjavabackend/TodoAppJavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.todoappjavabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppJavaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
