e0a4d2ca28a0a2194e9a8bfd1c27e2668411f66f
Rinae Hyun <rnhyun.dream@gmail.com>
2024-08-04 17:27:00 +0200

    Create GET by Id and PUT Methods with UpdatedTodoDto, correct NewTodoDto

--- src/main/java/org/example/todoappjavabackend/controller/TodoController.java
+++ src/main/java/org/example/todoappjavabackend/controller/TodoController.java
@@ -2,11 +2,13 @@ package org.example.todoappjavabackend.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.todoappjavabackend.dto.NewTodoDto;
+import org.example.todoappjavabackend.dto.UpdateTodoDto;
 import org.example.todoappjavabackend.model.Todo;
 import org.example.todoappjavabackend.service.TodoService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -23,6 +25,18 @@ public class TodoController {
     @GetMapping("/todo")
     public List<Todo> getAllTodos() {
         return todoService.retrieveAllTodos();
+    }
+
+    @GetMapping("/todo/{id}")
+    public Optional<Todo> getTodoById(@PathVariable String id) {
+        return todoService.retrieveTodoById(id);
+    }
+
+    @PutMapping("/todo/{id}")
+    public Optional<Todo> updateTodo(
+            @PathVariable String id,
+            @RequestBody UpdateTodoDto updateTodoDto) {
+        return todoService.updateTodoById(id, updateTodoDto);
     }
 
     /* TODO: check endpoints again
--- src/main/java/org/example/todoappjavabackend/dto/NewTodoDto.java
+++ src/main/java/org/example/todoappjavabackend/dto/NewTodoDto.java
@@ -1,9 +1,6 @@
 package org.example.todoappjavabackend.dto;
 
-import org.example.todoappjavabackend.model.TodoStatus;
 
 public record NewTodoDto(
-        int todoNumber,
-        String description,
-        TodoStatus status) {
+        String description) {
 }
