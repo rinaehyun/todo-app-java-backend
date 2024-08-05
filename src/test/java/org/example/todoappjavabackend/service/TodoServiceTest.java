e2bf0d5ca0b44a77ed6b4591614918402a9a167f
Rinae Hyun <rnhyun.dream@gmail.com>
2024-08-05 10:59:50 +0200

    Write tests for retrieveTodoById method

--- src/test/java/org/example/todoappjavabackend/service/TodoServiceTest.java
+++ src/test/java/org/example/todoappjavabackend/service/TodoServiceTest.java
@@ -7,8 +7,10 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -47,6 +49,41 @@ class TodoServiceTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void retrieveTodoById_whenTodoInDB_thenReturnTodo() {
+        // GIVEN
+        Todo todo1 = new Todo("123", "Cooking", TodoStatus.OPEN);
+        Todo todo2 = new Todo("456", "Jogging", TodoStatus.DONE);
+        List<Todo> todos = List.of(todo1, todo2);
 
+        String id = "123";
+        Todo retrievedTodo = todos.stream().filter(todo -> todo.id().equals(id)).toList().get(0);
+        when(todoService.retrieveTodoById(id)).thenReturn(Optional.ofNullable(retrievedTodo));
 
+        // WHEN
+        Optional<Todo> actual = todoService.retrieveTodoById(id);
+
+        // THEN
+        assert retrievedTodo != null;
+        Optional<Todo> expected = Optional.of(retrievedTodo);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void retrieveTodoById_whenNoTodoInDB_thenReturnNothing() {
+        // GIVEN
+        Todo todo1 = new Todo("123", "Cooking", TodoStatus.OPEN);
+        Todo todo2 = new Todo("456", "Jogging", TodoStatus.DONE);
+        List<Todo> todos = List.of(todo1, todo2);
+
+        String id = "789";
+        when(todoService.retrieveTodoById(id)).thenReturn(Optional.empty());
+
+        // WHEN
+        Optional<Todo> actual = todoService.retrieveTodoById(id);
+
+        // THEN
+        Optional<Todo> expected = Optional.empty();
+        assertEquals(expected, actual);
+    }
 }
